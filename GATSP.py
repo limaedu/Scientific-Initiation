e03b22f607970ea85337235c67ec8af2cfdcad85
limaedu <lima.edu@poli.ufrj.br>
2019-03-18 23:39:03 -0300

    Update GATSP.py

--- GATSP.py
+++ GATSP.py
@@ -9,23 +9,16 @@ tamanhoPop = 8
 
 
 
-class Cidade(self,numero,lat,long):
-    self.numero = numero
-    self.lat = lat
-    self.long = long
+class Cidade:
+    def __init__(self,numero,nome,lat,long):
+        self.nome = nome
+        self.numero = numero
+        self.lat = lat
+        self.long = long
     
     def __repr__(self):
-        return self.numero
+        return str(self.numero)
 
-def distancia(self,cidade):
-        distX = abs(self.x - cidade.x)
-        distY = abs(self.y - cidade.y)
-        distancia = np.sqrt(distX**2 + distY**2)
-        return distancia
-           
-
-def __repr__(self):
-        return   self.numero
     
 
 def GerarCromossomo(ListaDeCidades):
@@ -110,7 +103,7 @@ def Roleta(populacao):
     ranked = FitnessPercent(populacao)
     for j in range(len(ranked)):
         valoresfitness.append(ranked[j][1]) 
-    print (valoresfitness)    
+    #print (valoresfitness)    
     for i in range(len(ranked)):
         if i == 0:
             intervalo[ranked[i][0]] = (0.0,ranked[i][1])
@@ -128,20 +121,22 @@ def Selecao(populacao):
     roleta = Roleta(populacao)
     for _ in range(len(roleta)):
         aleatorio = random.uniform(0,100)
-        print (aleatorio)
+        #print (aleatorio)
         for i in roleta:
             interval = roleta[i]
             if aleatorio > interval[0] and aleatorio < interval[1]:
                 selecionados.append(i)
     
-
-    return selecionados   
+    
+    print ("SELECIONADOS: ")
+    return selecionados
+       
 
 
 def CrossoverOrdenado(parent1,parent2):
     
-    filho1 = np.zeros((1,6), dtype=np.float64)
-    filho2 = np.zeros((1,6), dtype = np.float64)
+    filho1 = np.zeros((1,len(parent1)), dtype=np.float64)
+    filho2 = np.zeros((1,len(parent1)), dtype = np.float64)
     
     
     while True:
@@ -153,60 +148,82 @@ def CrossoverOrdenado(parent1,parent2):
     inicio = min(gene1,gene2)
     fim = max(gene1,gene2)
     
-    print (inicio,fim)
+    #print (inicio,fim)
     
     for i in range(inicio,fim):
-      print (i)
-      filho1[0][i] = parent1[i]
-      filho2[0][i] = parent2[i]
+      #print (i)
+      filho1[0][i] = parent1[i].numero
+      filho2[0][i] = parent2[i].numero
     
     
     for i in range(-(len(parent2)-fim),fim):
-      print (i)
       for j in range(-(len(parent2)-fim),fim):
-        if filho1[0][i] == 0 and parent2[i] not in filho1:
+        if filho1[0][i] == 0 and parent2[i].numero not in filho1:
           if filho1[0][j] == 0:
-            filho1[0][j] = parent2[i]
-        if filho2[0][i] == 0 and parent1[i] not in filho2:
+            filho1[0][j] = parent2[i].numero
+        if filho2[0][i] == 0 and parent1[i].numero not in filho2:
           if filho2[0][j] == 0:
-            filho2[0][j] = parent1[i]
+            filho2[0][j] = parent1[i].numero
     
     
-    print (filho1)
+    #print (filho1)
     
     for i in range(len(parent2)):
       for j in range(len(parent2)):
         if filho1[0][i] == 0 and filho1[0][i] not in parent2:
-          if parent2[j] not in filho1:
-            filho1[0][i] = parent2[j]
+          if parent2[j].numero not in filho1:
+            filho1[0][i] = parent2[j].numero
         if filho2[0][i] == 0 and filho2[0][i] not in parent1:
-          if parent1[j] not in filho2:
-              filho2[0][i] = parent1[j]
-    
+          if parent1[j].numero not in filho2:
+              filho2[0][i] = parent1[j].numero
+  
     return (filho1,filho2)
 
 
 
+def CruzarSelecionados(selecionados,PopInicial):
+    filhos = []
+    selecionados_int = []
+    for i in range(len(selecionados)):
+        numero = int(selecionados[i].split(" ")[1])
+        print (PopInicial[numero-1])
+        selecionados_int.insert(i,PopInicial[numero-1])  #Descobrir se o número da rota está na ordem
+        print (numero)                                   #da População Inicial
+    
+    
+        
+            
+        
+        
+    
+    
+    return filhos
+       
 
-         
-
-C1 = Cidade("1",-22.908892,-43.177138)
-C2 = Cidade("2",-23.550483,-46.633106)
-C3 = Cidade("3",-30.033914,-51.229154)
-C4 = Cidade("4",-20.319933,-40.336296)
-C5 = Cidade("5",-7.120034,-34.876211)
-C6 = Cidade("6",-25.433171,-49.27147)
-C7 = Cidade("7",-27.593237,-48.543736)
+C1 = Cidade(1,"RJ",22.908892,-43.177138)
+C2 = Cidade(2,"SP",-23.550483,-46.633106)
+C3 = Cidade(3,"MG",-30.033914,-51.229154)
+C4 = Cidade(4,"PA",-20.319933,-40.336296)
+C5 = Cidade(5,"PR",-7.120034,-34.876211)
+C6 = Cidade(6,"SC",-25.433171,-49.27147)
+C7 = Cidade(7,"RS",-27.593237,-48.543736)
 Lista = [C1,C2,C3,C4,C5,C6,C7]
-I1 = GerarCromossomo(Lista)
-
-print (I1)
+#I1 = GerarCromossomo(Lista)
+#I2 = GerarCromossomo(Lista)
+#print (I1)
+#print (I2)
 
 
 
 P1 = (GerarPopInicial(tamanhoPop,Lista))
-
-print (FitnessPercent(P1))
-print (Roleta(P1))
-print (Selecao(P1))
-#print (CrossoverOrdenado(I1,I1))
+print (P1)
+s =  (Selecao(P1))
+print (s)
+print (str(len(P1)) + "oi")
+#print (P1)
+#print (FitnessPercent(P1))
+#print (Roleta(P1))
+#s =  (Selecao(P1))
+#array = (CrossoverOrdenado(I1,I2))
+#print (array)
+print (CruzarSelecionados(s,P1))
